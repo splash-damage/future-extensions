08ab96b7e7854ca9fc09a0bda42e6507a0d5a77e
DomCurry <hello@domcurry.com>
2020-09-23 18:43:41 +0100

    Add back SD namespaces

--- Source/SDFutureExtensions/Public/FutureExtensionsStaticFuncs.h
+++ Source/SDFutureExtensions/Public/FutureExtensionsStaticFuncs.h
@@ -4,7 +4,7 @@
 namespace SD
 {
 	template<typename F>
-	auto Async(F&& Function, const FExpectedFutureOptions& FutureOptions = FExpectedFutureOptions())
+	auto Async(F&& Function, const SD::FExpectedFutureOptions& FutureOptions = SD::FExpectedFutureOptions())
 	{
 		return FutureInitialisationDetails::CreateExpectedFuture(Forward<F>(Function), FutureOptions);
 	}
