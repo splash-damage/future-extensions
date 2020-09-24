0e09886a19a859dfa3a4683dbe5925f838d27ae9
DomCurry <hello@domcurry.com>
2020-09-24 13:32:02 +0100

    Add the failure options for WhenAll

--- Source/SDFutureExtensions/Private/FutureExtensionsStaticFuncs.cpp
+++ Source/SDFutureExtensions/Private/FutureExtensionsStaticFuncs.cpp
@@ -1,30 +1,60 @@
 #pragma once
 
 #include "FutureExtensions.h"
+#include <atomic>
 
-SD::TExpectedFuture<void> SD::WhenAll(const TArray<SD::TExpectedFuture<void>>& Futures)
+SD::TExpectedFuture<void> SD::WhenAll(const TArray<SD::TExpectedFuture<void>>& Futures, const EFailMode FailMode)
 {
 	if (Futures.Num() == 0)
 	{
 		return SD::MakeReadyFuture();
 	}
 
-	auto PromiseRef = MakeShared<SD::TExpectedPromise<void>, ESPMode::ThreadSafe>();
-	auto CounterRef = MakeShared<int32>(Futures.Num());
+	struct FSharedExpected
+	{
+		void Set(const TExpected<void>& Other) { Expected = Other; }
+		TExpected<void> Expected;
+	};
+
+	const auto CounterRef = MakeShared<std::atomic<int32>, ESPMode::ThreadSafe>(Futures.Num());
+	const auto PromiseRef = MakeShared<SD::TExpectedPromise<void>, ESPMode::ThreadSafe>();
+	const auto FirstErrorRef = MakeShared<FSharedExpected, ESPMode::ThreadSafe>();
 	for (auto& Future : Futures)
 	{
-		Future.Then([CounterRef, PromiseRef](SD::TExpected<void> Result)
+		Future.Then([CounterRef, PromiseRef, FirstErrorRef, FailMode](const SD::TExpected<void>& Result)
 			{
-				if (Result.IsCompleted())
+				--(CounterRef.Get());
+				if (Result.IsCompleted() == false)
 				{
-					if (--(CounterRef.Get()) == 0)
+					if (FirstErrorRef->Expected.GetState() == EExpectedResultState::Incomplete)
+					{
+						if (Result.IsError())
+						{
+							FirstErrorRef->Set(MakeErrorExpected<void>(Result.GetError().Get()));
+						}
+						else if (Result.IsCancelled())
+						{
+							FirstErrorRef->Set(MakeCancelledExpected<void>());
+						}
+					}
+
+					if (FailMode == EFailMode::Fast)
+					{
+						//Copy the expected value here as other continuations may still use it.
+						PromiseRef->SetValue(TExpected<void>(FirstErrorRef->Expected));
+					}
+				}
+
+				if (CounterRef.Get() == 0)
+				{
+					if (FirstErrorRef->Expected.GetState() == EExpectedResultState::Incomplete)
 					{
 						PromiseRef->SetValue();
 					}
-				}
-				else
-				{
-					PromiseRef->SetValue(MoveTemp(Result));
+					else
+					{
+						PromiseRef->SetValue(MoveTemp(FirstErrorRef->Expected));
+					}
 				}
 			});
 	}
