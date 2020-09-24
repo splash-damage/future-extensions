d7988869703bf9c3a3f99414adca49e280e91241
DomCurry <hello@domcurry.com>
2020-09-24 10:58:39 +0100

    responding to comments

--- Source/SDFutureExtensions/Private/FutureExtensionsStaticFuncs.cpp
+++ Source/SDFutureExtensions/Private/FutureExtensionsStaticFuncs.cpp
@@ -2,36 +2,6 @@
 
 #include "FutureExtensions.h"
 
-SD::TExpectedFuture<void> SD::WhenAll(TArray<SD::TExpectedFuture<void>>&& Futures)
-{
-	if (Futures.Num() == 0)
-	{
-		return SD::MakeReadyFuture();
-	}
-
-	auto PromiseRef = MakeShared<SD::TExpectedPromise<void>, ESPMode::ThreadSafe>();
-	auto CounterRef = MakeShared<int32>(Futures.Num());
-	for (auto& Future : Futures)
-	{
-		Future.Then([CounterRef = CounterRef, PromiseRef = PromiseRef](SD::TExpected<void> Result)
-			{
-				if (Result.IsCompleted())
-				{
-					if (--(CounterRef.Get()) == 0)
-					{
-						PromiseRef->SetValue();
-					}
-				}
-				else
-				{
-					PromiseRef->SetValue(MoveTemp(Result));
-				}
-			});
-	}
-
-	return PromiseRef->GetFuture();;
-}
-
 SD::TExpectedFuture<void> SD::WhenAll(const TArray<SD::TExpectedFuture<void>>& Futures)
 {
 	if (Futures.Num() == 0)
@@ -43,7 +13,7 @@ SD::TExpectedFuture<void> SD::WhenAll(const TArray<SD::TExpectedFuture<void>>& F
 	auto CounterRef = MakeShared<int32>(Futures.Num());
 	for (auto& Future : Futures)
 	{
-		Future.Then([CounterRef = CounterRef, PromiseRef = PromiseRef](SD::TExpected<void> Result)
+		Future.Then([CounterRef, PromiseRef](SD::TExpected<void> Result)
 			{
 				if (Result.IsCompleted())
 				{
@@ -59,5 +29,5 @@ SD::TExpectedFuture<void> SD::WhenAll(const TArray<SD::TExpectedFuture<void>>& F
 			});
 	}
 
-	return PromiseRef->GetFuture();;
+	return PromiseRef->GetFuture();
 }
