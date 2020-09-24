5e6f8ef620785041a554c7dd2a4d312d70bf07b7
DomCurry <hello@domcurry.com>
2020-09-24 10:57:38 +0100

    respond to comments

--- Source/SDFutureExtensions/Public/FutureExtensionsStaticFuncs.h
+++ Source/SDFutureExtensions/Public/FutureExtensionsStaticFuncs.h
@@ -3,46 +3,15 @@
 
 namespace SD
 {
-	template<typename F>
-	auto Async(F&& Function, const SD::FExpectedFutureOptions& FutureOptions = SD::FExpectedFutureOptions())
+	namespace Errors
 	{
-		return FutureInitialisationDetails::CreateExpectedFuture(Forward<F>(Function), FutureOptions);
+		constexpr int32 ERROR_INVALID_ARGUMENT = 1;
 	}
 
-	template<typename T>
-	SD::TExpectedFuture<TArray<T>> WhenAll(TArray<SD::TExpectedFuture<T>>&& Futures)
+	template<typename F>
+	auto Async(F&& Function, const FExpectedFutureOptions& FutureOptions = FExpectedFutureOptions())
 	{
-		if (Futures.Num() == 0)
-		{
-			return MakeReadyFuture<TArray<T>>(TArray<T>());
-		}
-
-		auto PromiseRef = MakeShared<SD::TExpectedPromise<TArray<T>>, ESPMode::ThreadSafe>();
-		auto ValueRef = MakeShared<TArray<T>, ESPMode::ThreadSafe>();
-		auto CounterRef = MakeShared<int32, ESPMode::ThreadSafe>(Futures.Num());
-		for (auto& Future : Futures)
-		{
-			Future.Then([CounterRef = CounterRef, PromiseRef = PromiseRef, ValueRef = ValueRef](const SD::TExpected<T>& Result)
-				{
-					if (Result.IsCompleted())
-					{
-						ValueRef->Emplace(Result.GetValue().GetValue());
-						if (--(CounterRef.Get()) == 0)
-						{
-							PromiseRef->SetValue(ValueRef.Get());
-						}
-					}
-					else if(Result.IsError())
-					{
-						PromiseRef->SetValue(Result.GetError().Get());
-					}
-					else if (Result.IsCancelled())
-					{
-						PromiseRef->SetValue(MakeCancelledExpected<TArray<T>>());
-					}
-				});
-		}
-		return PromiseRef->GetFuture();
+		return FutureInitialisationDetails::CreateExpectedFuture(Forward<F>(Function), FutureOptions);
 	}
 
 	template<typename T>
@@ -58,7 +27,7 @@ namespace SD
 		auto CounterRef = MakeShared<int32, ESPMode::ThreadSafe>(Futures.Num());
 		for (auto& Future : Futures)
 		{
-			Future.Then([CounterRef = CounterRef, PromiseRef = PromiseRef, ValueRef = ValueRef](const SD::TExpected<T>& Result)
+			Future.Then([CounterRef, PromiseRef, ValueRef](const SD::TExpected<T>& Result)
 				{
 					if (Result.IsCompleted())
 					{
@@ -81,38 +50,19 @@ namespace SD
 		return PromiseRef->GetFuture();
 	}
 
-	SDFUTUREEXTENSIONS_API SD::TExpectedFuture<void> WhenAll(TArray<SD::TExpectedFuture<void>>&& Futures);
 	SDFUTUREEXTENSIONS_API SD::TExpectedFuture<void> WhenAll(const TArray<SD::TExpectedFuture<void>>& Futures);
-
-	template<typename T>
-	SD::TExpectedFuture<T> WhenAny(TArray<SD::TExpectedFuture<T>>&& Futures)
-	{
-		if (Futures.Num() == 0)
-		{
-			return SD::MakeErrorFuture<T>(Error(1, TEXT("SD::WhenAny - Must have at least one element in the array.")));
-		}
-		auto PromiseRef = MakeShared<SD::TExpectedPromise<T>, ESPMode::ThreadSafe>();
-		for (auto& Future : Futures)
-		{
-			Future.Then([PromiseRef = PromiseRef](const SD::TExpected<T>& Result)
-				{
-					PromiseRef->SetValue(SD::TExpected<T>(Result));
-				});
-		}
-		return PromiseRef->GetFuture();
-	}
 
 	template<typename T>
 	SD::TExpectedFuture<T> WhenAny(const TArray<SD::TExpectedFuture<T>>& Futures)
 	{
 		if (Futures.Num() == 0)
 		{
-			return SD::MakeErrorFuture<T>(Error(1, TEXT("SD::WhenAny - Must have at least one element in the array.")));
+			return SD::MakeErrorFuture<T>(Error(Errors::ERROR_INVALID_ARGUMENT, TEXT("SD::WhenAny - Must have at least one element in the array.")));
 		}
 		auto PromiseRef = MakeShared<SD::TExpectedPromise<T>, ESPMode::ThreadSafe>();
 		for (auto& Future : Futures)
 		{
-			Future.Then([PromiseRef = PromiseRef](const SD::TExpected<T>& Result)
+			Future.Then([PromiseRef](const SD::TExpected<T>& Result)
 				{
 					PromiseRef->SetValue(SD::TExpected<T>(Result));
 				});
